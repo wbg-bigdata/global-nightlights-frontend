0aacea12536c5c5de1950d375970101bdb4aeb9f
Derek Lieu <dl@dereklieu.com>
2018-09-18 18:39:15 -0400

    Add about, data liks to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,21 @@
 // Modules
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render() {
     return (
-      <header id="site-header" className="clearfix" role="banner">
-        <h1 id="site-title">
-          <a href="#/" title="Global Night Lights">
+      <header id='site-header' className='clearfix' role='banner'>
+        <h1 id='site-title'>
+          <Link to='/' title='Global Night Lights'>
             <span>Global Night Lights</span>
-          </a>
+          </Link>
         </h1>
-        <nav id="site-prime-nav">
-          <ul className="global-menu"></ul>
+        <nav id='site-prime-nav'>
+          <ul className='global-menu'>
+            <li><Link to='about'>About</Link></li>
+            <li><a href='https://wbg-bigdata.github.io/global-nightlights-api/api/'>Data</a></li>
+          </ul>
         </nav>
       </header>
     );
